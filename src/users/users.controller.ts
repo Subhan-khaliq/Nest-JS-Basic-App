023c77d64274a3f1de9936ea8b0df1df5c140e2b
Subhan Khaliq <subhankhaliq420@gmail.com>
2022-10-11 18:22:20 +0500

    New Changes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,22 +1,42 @@
-import { Controller, Get, Post, Req, Redirect, Body, Param} from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Req,
+  Redirect,
+  Body,
+  Delete,
+  Param,
+  Put,
+} from '@nestjs/common';
 import { Request } from 'express';
 import { CreateUserDto } from './create-user.dto';
 
 @Controller('users')
 export class UsersController {
   @Post()
-  async create(@Body() createUserDto: CreateUserDto){
-    return `The record have been created. Details: #${createUserDto.username}`;
+  async create(@Body() createUserDto: CreateUserDto) {
+    return `The record have been created`;
   }
 
   @Get()
   @Redirect('https://nestjs.com', 301)
-  findAll(@Req() req: Request): string {
+  findAll(): string {
     return 'This action will show all the users';
   }
 
   @Get(':id')
   findone(@Req() req: Request): string {
     return `This user have #${req.params.id}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `The requested user has been deleted which has id of #${id}`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string) {
+    return `The requested user has been updated which has id of #${id}`;
   }
 }
