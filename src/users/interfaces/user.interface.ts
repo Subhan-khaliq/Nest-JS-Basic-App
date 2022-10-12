103bb442d924c63dc78277c761f9433b6c9d8b57
Subhan Khaliq <subhankhaliq420@gmail.com>
2022-10-12 12:42:48 +0500

    Changes

---
+++ src/users/interfaces/user.interface.ts
@@ -0,0 +1,5 @@
+export interface User {
+  username: string;
+  age: string;
+  email: string;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,4 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './interfaces/user.interface' ;
+
+
