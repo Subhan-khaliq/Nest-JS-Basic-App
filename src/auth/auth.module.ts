d52c77431f753101937e5b520503d00604572c00
Subhan Khaliq <subhankhaliq420@gmail.com>
2022-10-13 17:47:00 +0500

    Changes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,24 +1,25 @@
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
-import { UsersService } from 'src/user/user.service';
+import { UsersService } from 'src/users/users.service';
 import { LocalStrategy } from './local-strategy';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
+import { User } from 'src/users/entities/user.entity';
 import { AuthService } from './auth.service';
 import { Module } from '@nestjs/common';
-import { UserModule } from 'user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
-    UserModule,
+    UsersModule,
     PassportModule,
     JwtModule.register({
       secret: 'secretKey',
-    signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '60s' },
     }),
     TypeOrmModule.forFeature([User]),
   ],
   providers: [AuthService, UsersService, LocalStrategy],
   controllers: [AuthController],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotAcceptableException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+  async validateUser(user: CreateUserDto): Promise<any> {
+    const searchedUser = await this.usersService.findOne({ id });
+    if (!user) return null;
+    const passwordValid = await bcrypt.compare(
+      searchedUser.password,
+      user.password,
+    );
+    if (!user) {
+      throw new NotAcceptableException('could not find the user');
+    }
+    if (user && passwordValid) {
+      return user;
+    }
+    return null;
+  }
+  async login(user: any) {
+    const payload = { username: user.username, sub: user._id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
