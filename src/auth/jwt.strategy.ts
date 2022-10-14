61d9f8a47afa399087fa97bfd2608e16056d4b66
Subhan Khaliq <subhankhaliq420@gmail.com>
2022-10-14 20:10:07 +0500

    Changes

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,7 +1,6 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { jwtConstants } from './constants';
-
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
